270c573890f4ddfe63d7a67ace456dca2b408466
Legat <zia-d@mail.ru>
2022-01-07 19:11:34 +0400

    feat: make function work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
   
+  let number = Math.abs(n).toString();
+  let inversedNumber = '';
+
+  for (let i = number.length - 1; i >= 0; i--) {
+    inversedNumber += number[i];
+  }
+
+  return +inversedNumber;
+
 }
